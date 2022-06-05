e44ac4b91fa8a29a1b69624ececac7217cd2092c
A.J <8924517+AJ1701@users.noreply.github.com>
2022-06-05 13:05:00 -0500

    Support token authentication

--- authentication/auth.go
+++ authentication/auth.go
@@ -14,8 +14,14 @@ import (
 )
 
 func GetAuth(cfg *ini.File) string {
+	token := cfg.Section("api-authentication").Key("token").String()
 	email := cfg.Section("api-authentication").Key("email").String()
 	password := cfg.Section("api-authentication").Key("password").String()
+
+	if len(token) > 0 {
+		return token
+	}
+
 	values := map[string]string{"email": email, "password": password}
 
 	jsonValue, _ := json.Marshal(values)
