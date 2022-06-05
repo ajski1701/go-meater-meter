653350ae0951794d7f2c6d9fc63ad142d28a0727
A.J <8924517+AJ1701@users.noreply.github.com>
2022-06-05 01:27:53 -0500

    Cleanup

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # go-meater-meter
 
-## Configuration
-### Meater API Authentication (DRAFT)
+## Configuration - config.ini
 ```ini
-email = <email>
-password = <password>
-```
-### Postgres Authentication (DRAFT)
-```ini
-server = <server address>
-port = <server_port>
-username = <username>
-password = <password>
-database_name = <database_name>
+[api-authentication]
+email = "<meater_cloud_password>"
+password = "<meater_cloud_password>"
+
+[app-config]
+poll_rate = 30 #in seconds
+
+[influxdb]
+url = "<influxdb_url>"
+token = "<influxdb_token>" #INFLUXDB_TOKEN
+org = "<org>"
+bucket = "<bucket>"
 ```
 
 ## References
 ### API Spec
 https://github.com/apption-labs/meater-cloud-public-rest-api
-### lib/pq
-https://github.com/lib/pq/blob/master/example/listen/doc.go
-#### Connection string format
-`postgresql://<username>@<server>:<port>/<database_name>`
--- meater/devices/devices.go
+++ meater/devices/devices.go
@@ -11,7 +11,7 @@ func GetDevices(token string) []models.Devices {
 	var bearer = "Bearer " + token
 
 	url := "https://public-api.cloud.meater.com/v1/devices"
-	url = "http://127.0.0.1:8080/device_json"
+	//url = "http://127.0.0.1:8080/device_json"
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		panic(err)
@@ -32,21 +32,5 @@ func GetDevices(token string) []models.Devices {
 	var result models.Device
 	json.Unmarshal([]byte(body), &result)
 
-	// for _, device := range result.Data.Devices {
-	// 	localOutputMap := map[string]string{}
-
-	// 	localOutputMap["device_id"] = device.Id
-	// 	localOutputMap["temperature_ambient"] = device.Temperature.Ambient
-	// 	localOutputMap["temperature_internal"] = device.Temperature.Internal
-	// 	localOutputMap["cook_id"] = device.Cook.Id
-	// 	localOutputMap["cook_name"] = device.Cook.Name
-	// 	localOutputMap["cook_state"] = device.Cook.State
-	// 	localOutputMap["cook_target"] = device.Cook.Temperature.Target
-	// 	localOutputMap["cook_peak"] = device.Cook.Temperature.Peak
-	// 	localOutputMap["cook_time_elapsed"] = device.Cook.Time.Elapsed
-	// 	localOutputMap["cook_time_remaining"] = device.Cook.Time.Remaining
-	// 	localOutputMap["updated_at"] = device.Temperature.Internal
-	// 	outputArray = append(outputArray, localOutputMap)
-	// }
 	return result.Data.Devices
 }
